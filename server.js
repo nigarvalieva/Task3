016abe900b77dd9336fd8e85657444e23a288ab0
nigarvalieva <vvaliyevaa@gmail.com>
2022-12-15 07:50:09 +0400

    t3

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+let card = [
+    {
+        id: 1,
+        cardNum: "4249 7866 5657 1426"
+    },
+    {
+        id: 2,
+        cardNum: "4018 6662 0308 0148"
+    },
+    {
+        id: 3,
+        cardNum: "4006 2049 7347 6397"
+    },
+    {
+        id: 4,
+        cardNum: "4934 6861 5882 1461"
+    },
+    {
+        id: 5,
+        cardNum: "4744 6731 7265 7147"
+    }
+]
+
+app.get('/card', function (req, res) {
+    res.send("Send num from 1 to 5!")
+})
+
+app.get('/card/:id', function (req, res) {
+    card.forEach((cart) => {
+        if (cart.id === req.params.id) {
+            res.send(cart.cardNum) 
+        } 
+    })
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
